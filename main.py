4f1f3646c96e9f8d5bbee1073cd8dd5df53450d0
michellex574 <168604808+michellex574@users.noreply.github.com>
2024-05-07 08:43:17 -0400

    Day 2 Commit
    I decided that I would be needing more variables, and hence included them. I will need to set a timer, either from high to low or low to high, but I’ll decide that later. I also added a background image, but I’m not sure how to check if it looks right, so I will be doing that later as well, when I have at least part of my main code.

--- main.py
+++ main.py
@@ -14,17 +14,32 @@ SCREEN_WIDTH = 530
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 
-r = 219
-g = 215
-b = 210
-clicks = 0
+bg = pygame.image.load("bg.jpg")
+pancakes_eaten = 0
+waffles_eaten = 0
+start_time = round(time.time())
 
 # render the text for later
 message = "Use the left and right arrow keys to move the food!"
-pancakes = "pancakes eaten: "
-waffles = "waffles eaten: "
+pancakes = "pancakes eaten: " + str(pancakes_eaten)
+waffles = "waffles eaten: " + str(waffles_eaten)
 end_message = "Time's up! They sure are fed! "
 end_message2 = "Time's up! Did you starve them?.. "
 end_message3 = "Time's up! They're satisfied now that's for sure! "
 display_message = my_font.render(message, True, (0, 0, 0))
-start_time = time.time()
+display_pancakes = my_font.render(pancakes, True, (255, 255, 255))
+display_waffles = my_font.render(waffles, True, (255, 255, 255))
+run = True
+
+while run == True:
+    current_time = round((time.time() - start_time), 2)
+    display_time_elapsed = my_font.render("Time Elapsed: " + str(current_time), True, (255, 255, 255))
+    if time == 0:
+        run = False
+    pygame.display.update()
+
+if waffles_eaten + pancakes_eaten <= 4:
+    screen.blit(display_pancakes, (0, 0))
+    screen.blit(display_waffles, (0, 5))
+
+pygame.quit()
