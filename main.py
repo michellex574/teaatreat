e2ea60ed459ecc3b9ea06666a02e57558fbd6d2c
michellex574 <168604808+michellex574@users.noreply.github.com>
2024-05-13 08:41:27 -0400

    Day 5 Commit
    Regarding what I said about the drawings, once again, I did not finish the character drawings, but don’t worry, I will get it done sooner or later of course. I am coding for the movement of the pancake, waffle, and paffle today, and ideally figure out how to get the characters (to be replaced) to sit in one spot without moving but eat the food items when colliding with them.
    I finished coding for the movement of the pancake, waffle, and paffle, but I did not achieve the second part. Instead, I coded for the conditions that would cause a certain message to appear. 
    Goal for next class: Get the main screen running.

--- main.py
+++ main.py
@@ -22,17 +22,22 @@ screen = pygame.display.set_mode(size)
 p = Pancake(180, 180)
 w = Waffle(180, 180)
 b = Bunny(90, 180)
+c = Cat(270, 180)
+shown = random.randint(1,3)
+if shown == 1:
+    shown = p
+elif shown == 2:
+    shown = w
 r = 178
 g = 34
 b = 34
 background = Background(0 ,0)
 pancakes_eaten = 0
 waffles_eaten = 0
-start_time = round(time.time())
 
 
 # render the text for later
-message = "Use the left and right arrow keys to move the food!"
+message = "Use the a and d keys to move the food!"
 message2 = "The pancakes are for the bunny!"
 message3 = "The waffles are for the fox!"
 message4 = "Both the bunny and the fox enjoys the paffle, so it's up to you! "
@@ -49,6 +54,14 @@ display_message4 = my_font.render(message4, True, (0, 0, 0))
 display_message5 = my_font.render(message5, True, (0, 0, 0))
 display_pancakes = my_font.render(pancakes, True, (255, 255, 255))
 display_waffles = my_font.render(waffles, True, (255, 255, 255))
+display_end_message = my_font.render(end_message, True, (0, 0, 0))
+display_end_message2 = my_font.render(end_message2, True, (0, 0, 0))
+display_end_message3 = my_font.render(end_message3, True, (0, 0, 0))
+
+start_time = time.time()
+seconds_elapsed = 1
+time_appear = 0
+
 run = True
 intro_screen = True
 game_screen = False
@@ -81,17 +94,17 @@ while game_screen:
 
      keys = pygame.key.get_pressed()  # checking pressed keys
 
-     if keys[pygame.K_d]:
+     if keys[pygame.K_d] and shown == p:
          p.move_direction("right")
 
-     if keys[pygame.K_a]:
+     if keys[pygame.K_a] and shown == p:
          p.move_direction("left")
 
-     if keys[pygame.K_LEFT]:
-         p.move_direction("left")
-
-     if keys[pygame.K_RIGHT]:
+     if keys[pygame.K_d] and shown == w:
          p.move_direction("right")
+
+     if keys[pygame.K_a] and shown == w:
+         p.move_direction("left")
 
      if b.rect.colliderect(p.rect):
          eaten = "eaten detected"
@@ -101,15 +114,76 @@ while game_screen:
      elif b.rect.colliderect(w.rect):
          disgust = "disgust detected"
          display_disgust = my_font.render(disgust, True, (255, 255, 255))
-while run == True:
-     current_time = round((time.time() - start_time), 2)
-     display_time_elapsed = my_font.render("Time Elapsed: " + str(current_time), True, (255, 255, 255))
-     if time == 0:
-         run = False
+
+     if c.rect.colliderect(w.rect):
+         eaten = "eaten detected"
+         display_eaten = my_font.render(eaten, True, (255, 255, 255))
+         waffles = waffles + 5
+         display_score = my_font.render("Waffles: " + str(waffles), True, (255, 255, 255))
+     elif c.rect.colliderect(p.rect):
+         disgust = "disgust detected"
+         display_disgust = my_font.render(disgust, True, (255, 255, 255))
+
+     for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+            intro_screen = False
+            game_screen = False
+            end_screen = False
+
+     if seconds_elapsed >= 0:
+        screen.fill((r, g, b))
+        screen.blit(background.image, (0, 0))
+        screen.blit(display_pancakes, (1, 0))
+        screen.blit(display_waffles, (1, 15))
+        screen.blit(c.image, c.rect)
+        screen.blit(b.image, b.rect)
+        screen.blit(p.image, p.rect)
+        screen.blit(w.image, w.rect)
+        current_time = time.time()
+        seconds_elapsed = round((start_time - current_time) + 10, 2)
+        countdown = my_font.render("Time Elapsed: " + str(seconds_elapsed) + "s", True, (255, 255, 255))
+        screen.blit(countdown, (375, 0))
+
+     else:
+        game_screen = False
+        end_screen = True
+
      pygame.display.update()
 
-if waffles_eaten + pancakes_eaten <= 4:
-     screen.blit(display_pancakes, (0, 0))
-     screen.blit(display_waffles, (0, 5))
+while end_screen:
+    screen.fill((r, g, b))
+    if pancakes_eaten > 20:
+        display_end_message = my_font.render(end_message, True, (0, 0, 0))
+        screen.blit(display_end_message, (175, 165))
 
+    if pancakes_eaten < 5:
+        display_end_message2 = my_font.render(end_message2, True, (0, 0, 0))
+        screen.blit(display_end_message2, (175, 165))
+
+    if pancakes_eaten > 10 and pancakes_eaten < 15:
+        display_end_message2 = my_font.render(end_message3, True, (0, 0, 0))
+        screen.blit(display_end_message3, (175, 165))
+
+    if waffles_eaten > 20:
+        display_end_message = my_font.render(end_message, True, (0, 0, 0))
+        screen.blit(display_end_message, (175, 165))
+
+    if waffles_eaten < 5:
+        display_end_message2 = my_font.render(end_message2, True, (0, 0, 0))
+        screen.blit(display_end_message2, (175, 165))
+
+    if waffles_eaten > 10 and waffles_eaten < 15:
+        display_end_message2 = my_font.render(end_message3, True, (0, 0, 0))
+        screen.blit(display_end_message3, (175, 165))
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+            intro_screen = False
+            game_screen = False
+            end_screen = False
+
+    pygame.display.update()
+# Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
